887d78e4b5bb1ac46d669c53a134cfc761166c16
REMELH01 <rmelhornmusic@gmail.com>
2022-11-05 21:32:41 -0400

    csv added

--- kc_2/kc_2.py
+++ kc_2/kc_2.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-dataframe = pd.read_csv('National_Universities_Rankings')
+dataframe = pd.read_csv('National_Universities_Rankings.csv')
 
 Rank = dataframe('Rank')
 Tuition_and_Fees = dataframe('tuition_and_fees')
