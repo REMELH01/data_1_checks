4be1451ca1e4710f6ad35b84b555130a760d5679
REMELH01 <rmelhornmusic@gmail.com>
2022-11-05 20:25:46 -0400

    recommit

---
+++ assignment.py
@@ -0,0 +1,10 @@
+print('Hello world!')
+
+my_list = ['1', '2', '3', '4', '5']
+print(my_list[3])
+
+my_dict = {1:'flower', 2:'fruit', 3:'movies', 4:'books', 5:'paint'}
+print(my_dict[1])
+
+my_tuple = ('red', 'yellow', 'blue', 'green')
+print(my_tuple[0])
--- kc_2/kc_2.py
+++ kc_2/kc_2.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import seaborn as sns
 
-df = pd.read_csv(r'C:\Users\rmelh\OneDrive\Documents\data_1_check\kc_2\assets\chord_progressions.csv')
+df = pd.read_csv(r'C:\Users\rmelh\OneDrive\Documents\data_1_check\kc_2\assets\National_Universities_Rankings.csv')
 
-sns.scatterplot(x=df['"1st_chord"'],
-                y=df['"2nd_chord"'])
+sns.scatterplot(x=df['rank'],
+                y=df['tuition_and_fees'])
